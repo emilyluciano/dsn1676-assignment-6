2df978dc37d63b6a73bf58b25c9feca1c8c95c22
emilyluciano <emilydianeluciano@hotmail.com>
2015-03-05 16:12:56 -0500

    added the bouncing circle to js and css

--- js/main.js
+++ js/main.js
@@ -7,6 +7,13 @@ var $diamond =$('.diamond');
 var $btnCollapseExpand =$('.btn-collapse-expand');
 var $panel =$('.panel');
 
+var $btnBouncein =$('.btn-bounce');
+var $circle =$('.circle');
+
+var $appendexpand =$('.append-expand');
+var $list =$('.list');
+
+
 $btnShowHide.on('click', function () {
     $box.toggleClass('js-show-hide');
 });
@@ -18,5 +25,15 @@ $btnMove.on('click', function () {
 $btnCollapseExpand.on('click', function () {
  $panel.toggleClass('js-collapse-expand');
 });
+                
+$btnBouncein.on('click', function () {
+ $circle.addClass('js-bounce-in');
+});
 
+$circle.on('webkitAnimationEnd animationend', function () {
+    $circle.removeClass('js-bounce-in');
+});
 
+$list.on('click', function () {
+   
+});
